61105098cee1d3eef758c90ee9f87bc0cfd6c2f6
anghelo523 <lucariotorresarias@gmail.com>
2025-06-03 11:14:27 -0500

    AVANCE

---
+++ SEGUNDA_UNIDAD/SysVentas_login/src/main/java/pe/edu/upeu/sysventas/servicio/UnidadMedidaService.java
@@ -0,0 +1,54 @@
+package pe.edu.upeu.sysventas.servicio;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pe.edu.upeu.sysventas.dto.ComboBoxOption;
+import pe.edu.upeu.sysventas.modelo.Marca;
+import pe.edu.upeu.sysventas.modelo.UnidadMedida;
+import pe.edu.upeu.sysventas.repositorio.UnidadMedidaRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UnidadMedidaService {
+
+    @Autowired
+    private UnidadMedidaRepository unidadMedidaRepository;
+
+    // Create
+    public UnidadMedida guardarEntidad(UnidadMedida to) {
+        return unidadMedidaRepository.save(to);
+    }
+
+    // Report
+    public List<UnidadMedida> listarEntidad() {
+        return unidadMedidaRepository.findAll();
+    }
+
+    // Update
+    public UnidadMedida actualizarEntidad(UnidadMedida to) {
+        return unidadMedidaRepository.save(to);
+    }
+
+    // Delete
+    public void eliminarRegEntidad(Long id) {
+        unidadMedidaRepository.deleteById(id);
+    }
+
+    // Buscar por ID
+    public UnidadMedida buscarEntidad(Long id) {
+        return unidadMedidaRepository.findById(id).orElse(null);
+    }
+    public List<ComboBoxOption> listarCombobox(){
+        List<ComboBoxOption> listar=new ArrayList<>();
+        ComboBoxOption cb;
+        for(UnidadMedida um : unidadMedidaRepository.findAll()) {
+            cb=new ComboBoxOption();
+            cb.setKey(String.valueOf(um.getIdUnidad()));
+            cb.setValue(um.getNombreMedida());
+            listar.add(cb);
+        }
+        return listar;
+    }
+}
