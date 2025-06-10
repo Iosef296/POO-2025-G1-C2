6261b2e1ea0e6e78a61f842ed97fa25cb26eb0a7
anghelo523 <lucariotorresarias@gmail.com>
2025-06-10 08:18:29 -0500

    PROYECTO

---
+++ SEGUNDA_UNIDAD/prototipo/src/SistemasVentas.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class SistemasVentas {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sistema de Ventas Cafetria Pradoc")
+
+    }
+}
